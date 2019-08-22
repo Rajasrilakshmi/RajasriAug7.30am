61b0f3445590552c8a86c8f2a02270dbf0ca99f7
Rajasrilakshmi <rajasrilakshmir>
2019-08-22 08:47:41 +0530

    Fibonacci program

---
+++ src/org/test/Fibonacci.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Fibonacci {
+public static void main(String[] args) {
+	int n=10,a=0,b=1;
+	for (int i = 1; i <= n; i++) {
+		System.out.println(a);
+		int sum = a+b;
+		a=b;
+		b=sum;
+	}
+}
+}
